ec4587629b0b7611896b622adac3a754a4ab5943
Javier Jimenez <javier.sniguel@gmail.com>
2025-04-10 00:57:44 -0300

    Creamos las vetansa agregar animla, menu y los comboBox con datos

--- src/domain/Pais.java
+++ src/domain/Pais.java
@@ -1,32 +1,20 @@
 package domain;
 
 public class Pais {
-<<<<<<< HEAD
-    
-    private String nombre;
 
-    public Pais(String nombre) {
-        this.nombre = nombre;
-=======
     private final String nombre;
     private final String codigoIso;
 
     public Pais(String nombre, String codigoIso) {
         this.nombre = nombre;
         this.codigoIso = codigoIso;
->>>>>>> pais-origen
     }
 
     public String getNombre() {
         return nombre;
     }
-<<<<<<< HEAD
     
-    
-=======
-
     public String getCodigoIso() {
         return codigoIso;
     }
->>>>>>> pais-origen
 }
